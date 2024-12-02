85da9e9e9205aa6c7d2c4e52cfd80278947be65f
MohammedQays <m.k.alaneeq@gmail.com>
2024-12-02 21:16:09 +0300

    Update runner.py
    54000

--- runner.py
+++ runner.py
@@ -5,8 +5,7 @@ import subprocess
 print("تشغيل البوت من سكربت runner.py")
 process = subprocess.Popen(["python", "wikipedia_stub_bot.py"])
 
-# الاستمرار في العمل لمدة ساعتين (7200 ثانية)
-time.sleep(10800)  # التوقف بعد ساعتين
+time.sleep(54000)  # التوقف بعد 15 ساعة
 
 # طباعة رسالة عند انتهاء الفترة
 print("تم إيقاف البوت بعد مرور ساعتين.")
