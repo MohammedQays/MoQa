010c39edd0ced963342b4ffb5478630b9c76cf40
MohammedQays <m.k.alaneeq@gmail.com>
2024-12-10 00:42:48 +0300

    Update runner.py

--- runner.py
+++ runner.py
@@ -3,7 +3,7 @@ import subprocess
 
 # تشغيل سكربت البوت الأساسي
 print("تشغيل البوت من سكربت runner.py")
-process = subprocess.Popen(["python", "wikipedia_stub_bot.py"])
+process = subprocess.Popen(["python", "phtobot.py"])
 
 time.sleep(54000)  # التوقف بعد 15 ساعة
 
